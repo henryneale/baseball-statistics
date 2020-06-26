4d0104f9036a32284bc9426cd476cb920e7c4f5a
Henry Neale <hyneale@gmail.com>
2020-06-26 13:47:38 -0400

    Change database to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_06_26_171911) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.float "avg"
